c68c40c12e64c6d8fbd12b0b8f354aeead02f94b
Taylor Otey <taylorotey5@gmail.com>
2018-08-17 13:59:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,50 @@
+require './config/environment'
+require './app/models/quiz.rb'
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    return erb :index
+  end
+  
+  get '/taylor' do
+    return erb :taylor
+  end
+  
+  get '/isabel' do
+    return erb :isabel
+  end
+  
+  get '/jiaming' do
+    return erb :jiaming
+  end
+  
 
+  post '/results' do
+
+  points_array = params.values 
+  
+    total = 0
+    points_array.each do |point| 
+      total += point.to_i 
+    end
+
+    result = calculate_result(total) 
+    
+    
+    if result == "jiaming" 
+      return erb :jiaming
+    elsif result == "taylor"
+      return erb :taylor
+    elsif result == "isabel"
+      return erb :isabel
+    end
+    
+    
+  end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,9 @@
-class Quiz
-
+def calculate_result(point_total)
+    if point_total <= 15 && point_total >= 0
+        return "jiaming"
+    elsif point_total >=16 && point_total <= 30
+        return "taylor"
+    else
+        return "isabel"
+    end
 end
